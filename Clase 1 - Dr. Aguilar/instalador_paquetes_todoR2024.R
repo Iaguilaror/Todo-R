bab03a25d80b207a8f5b119613c6ee34cd3ed64b
Lab-Aguilar <iaguilaror@gmail.com>
2024-11-07 11:36:28 -0600

    Delete Clase 1 - Dr. Aguilar directory

--- Clase 1 - Dr. Aguilar/instalador_paquetes_todoR2024.R
+++
@@ -1,7 +0,0 @@
-if ( !require("pacman") ) install.packages( "pacman" )
-
-pacman::p_load( "gsheet", "readtext", "DT", "tidyverse",
-                "tidytext", "dplyr", "stringr", "wordcloud",
-                "ggplot2", "pdftools", "tidyr","readr", "stopwords",
-                "scales", "RColorBrewer", "viridis" )
-
